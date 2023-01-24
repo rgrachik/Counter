0d1a9fa293b5ba4363e9ac7ae1075bc663af1cd8
rgrachik <112074005+rgrachik@users.noreply.github.com>
2023-01-24 17:58:08 +0300

    First
    Minimum functional

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var counter = 0
+    
 
+    @IBOutlet weak var countLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counter = 0
+        countLabel.text = "\(counter)"
     }
 
-
+    @IBAction func plusButtonPressed() {
+        counter += 1
+        countLabel.text = "\(counter)"
+    }
+    
+    @IBAction func resetButtonPressed() {
+        counter = 0
+        countLabel.text = "\(counter)"
+    }
+    
+    
 }
 
